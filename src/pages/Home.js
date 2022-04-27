23c4622f435eee76cd2b2a678a5dcb43d9d131f4
unknown <aghazjunaid96@gmail.com>
2022-04-27 13:08:22 +0530

    home page user listing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,38 @@
+import React, {useState, useEffect} from "react";
 
 
 const Home = () => {
+    const [data, setData] = useState([])
+    function getUsers(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(result => setData(result))
+        .catch(err => console.log(err))
+    }
+
+    useEffect(()=>{
+        getUsers()
+    },[])
+    console.log(data)
+
     return (
         <>
-            Home
+            <div style={{textAlign:"center"}}>
+                <button>Accepted List</button>
+                <button>Rejected List</button>
+            </div>
+            {data.length > 0 && data.map((ele,index) => {
+                return (
+                    <div key={index}>
+                        <h3>Name : {ele.name}</h3>
+                        <div>Id : {ele.id}</div>
+                        <div>Email : {ele.email}</div>
+                        <div>Phone : {ele.phone}</div>
+                        <div>Website : {ele.website}</div>
+                    </div>
+                )
+            })
+            }
         </>
     )
 }
